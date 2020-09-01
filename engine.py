a7ddb37f813156137151c3366517dd4d409cb6d3
FilipePrates <filipeprates@poli.ufrj.br>
2020-09-01 20:59:54 -0300

    some more tests using chess library

--- engine.py
+++ engine.py
@@ -1,3 +1,13 @@
+# from stockfish import Stockfish
+#
+# stockfish = Stockfish("stockfish\stockfish-11-win\Windows\stockfish_20011801_x64")
+# stockfish.set_fen_position("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
+# print(stockfish.get_board_visual())
+# print('melhor move:')
+# print(stockfish.get_best_move())
+#
+# # print(stockfish)
+
 # import chess
 # import chess.uci
 # print("Hello World")
@@ -17,20 +27,37 @@ import chess.engine
 engine = chess.engine.SimpleEngine.popen_uci("stockfish\stockfish-11-win\Windows\stockfish_20011801_x64")
 
 board = chess.Board()
+
+
 while not board.is_game_over():
+    analysis = engine.analysis(board,chess.engine.Limit(time=0.1),info='INFO_ALL')
     result = engine.play(board, chess.engine.Limit(time=0.1))
+    print('ãnalysis',analysis.info)
+    print(result)
     board.push(result.move)
     print(board)
     print("--------------------")
 
 engine.quit()
 
-from stockfish import Stockfish
+import asyncio
+import chess
+import chess.engine
 
-stockfish = Stockfish("stockfish\stockfish-11-win\Windows\stockfish_20011801_x64")
-stockfish.set_fen_position("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
-print(stockfish.get_board_visual())
-print('melhor move:')
-print(stockfish.get_best_move())
+async def main() -> None:
+    board = chess.Board()
+    transport, engine = await chess.engine.popen_uci("stockfish\stockfish-11-win\Windows\stockfish_20011801_x64")
+    print(board)
+    with await engine.analysis(board) as analysis:
+        async for info in analysis:            # Arbitrary stop condition.
+            if info.get("seldepth", 0) == 10:
+                print(info.get("pv"))
+                for move in info.get("pv"):
+                     board.push(move)
+                     print(move)
+                break
 
-# print(stockfish)
+    await engine.quit()
+
+asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+asyncio.run(main())
