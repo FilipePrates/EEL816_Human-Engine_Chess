a7ed9aba680e56e7b7dc6d077e18b8682a9b88b6
FilipePrates <filipeprates@poli.ufrj.br>
2020-09-03 22:22:14 -0300

    liso

--- engine.py
+++ engine.py
@@ -5,35 +5,49 @@ import copy
 receivedPiece = 'P'
 receivedBoard = '1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - 0 1'
 receivedBoard2 = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
+
 engine = chess.engine.SimpleEngine.popen_uci("stockfish\stockfish-11-win\Windows\stockfish_20011801_x64")
 board = chess.Board(receivedBoard2)
+
+
 print("Board Inicial")
 print(board)
-print(board.legal_moves)
 
-receivedPiece = input('Escolha a peça (ex. P, N, k, Q, R, r,..)')
-bestMove = {"cp":chess.engine.Cp(0),"move":""}
+def chooseMove(board, piece):
+    print(board.legal_moves)
+    for move in board.legal_moves:
+        if(board.piece_at(move.from_square).symbol().lower() == piece.lower()):
+            # print('Analisando: ',move.uci())
+            # print('Piece: ',board.piece_at(move.from_square))
+            board.push(move)
+            # print(board)
 
-for move in board.legal_moves:
-    if(board.piece_at(move.from_square).symbol() == receivedPiece):
-        print('Move: ',move.uci())
-        print('Piece: ',board.piece_at(move.from_square))
-        boardAux = copy.deepcopy(board)
-        boardAux.push(move)
-        print(boardAux)
-        analysis = engine.analysis(boardAux,chess.engine.Limit(time=0.3,depth=10))
-        analysis.get()
-        print('haha',analysis.info['score'].white())
-        if(bestMove['cp'].score() > analysis.info['score'].pov(analysis.info['score'].white()).score()):
-            bestMove['cp'] = analysis.info['score'].pov(False)
-            bestMove['move'] = move
-        print('analysis:',analysis.info['score'])
-        print("------------------")
+            analysis = engine.analysis(board,chess.engine.Limit(time=0.3,depth=15))
+            analysis.get()
 
-print('BestMove:',bestMove['move'].uci())
-board.push(bestMove['move'])
-print(board)
+            # print('Best Score so far',bestMove['cp'].score())
+            # print('Score',analysis.info['score'].pov(analysis.info['score'].turn).score())
+            if(bestMove['cp'].score() < analysis.info['score'].pov(analysis.info['score'].turn).score()):
+                bestMove['cp'] = analysis.info['score'].pov(analysis.info['score'].turn)
+                bestMove['move'] = move
+            # print('analysis:',analysis.info['score'])
+            # print("------------------")
+            board.pop()
 
+    print('BestMove:',bestMove['move'].uci())
+    board.push(bestMove['move'])
+    print(board)
+
+
+while(not board.is_game_over()):
+    print("===============")
+    if(board.turn):
+        receivedPiece = input('Escolha a peça (ex. P, N, K, Q, R, R,..)')
+    else:
+        receivedPiece = input('Escolha a peça (ex. p, n, k, q, r, r,..)')
+    bestMove = {"cp":chess.engine.Cp(-10000),"move":chess.Move.null()}
+    chooseMove(board,receivedPiece)
 
 
 
